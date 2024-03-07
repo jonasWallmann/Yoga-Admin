0047de968ab6ed99f04ed7083249300b43945259
Jonas Wallmann <jonas.wallmann@t-online.de>
2024-03-07 18:06:58 +0100

    Initial Commit

---
+++ Yoga Admin/Yoga_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yoga_AdminApp.swift
+//  Yoga Admin
+//
+//  Created by Jonas Wallmann on 07.03.24.
+//
+
+import SwiftUI
+
+@main
+struct Yoga_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
